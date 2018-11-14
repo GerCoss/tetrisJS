3d879872ca5134bbc0f92fa39a43f17f22bc36ef
GerCoss <gerardocoss92@gmail.com>
2018-11-14 14:06:57 -0600

    tetris primer commit

---
+++ js/juego.js
@@ -0,0 +1,36 @@
+var canvas;
+var ctx;
+var FPS = 50;
+
+var anchoCanvas = 400;
+var altoCanvas = 640;
+
+var anchoTablero = 10;
+var altoTablero = 16;
+
+var anchoF = 40;
+var altoF = 40;
+
+var tablero = [
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+];
+
+function inicializa() {
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+
+    canvas.style.width = anchoCanvas;
+    canvas.style.height = altoCanvas;
+}
